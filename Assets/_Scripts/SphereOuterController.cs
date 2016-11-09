27f2c07543aedd626bad1039f5626833d3bdc6b1
theonlyjames <jkhatiblou@gmail.com>
2016-11-09 12:10:49 -0800

    cleaning up code

--- Assets/_Scripts/SphereOuterController.cs
+++ Assets/_Scripts/SphereOuterController.cs
@@ -2,20 +2,40 @@
 using System.Collections;
 using UnityEngine.UI;
 
+/// <summary>
+/// Sphere outer controller.
+/// 
+/// This ended up being far more convoluted than it neeeded to be. 
+/// I just wanted to get something quick and dirty to you guys. 
+/// 
+/// Lets Discuss when you have a chance. 
+/// 
+/// Instructions: 
+/// 
+/// Click anywhere on the screen with a mouse and the "Cursor" object will fly to the cursor. 
+/// When the Cursor approaches the mouse and increase in drag is added until it stops within a threshold.
+/// When it passes the threshold and you keep mousedown it will snap onto the mouse and manipulation of
+/// the objects transform will be used instead of adding force to attract it. 
+/// 
+/// You can then use the "Cursor" object to pass through the translucent outer sphere (I could have just used the 
+/// Sphere Collider here) but instead went this route. 
+/// 
+/// </summary>
+
 public class SphereOuterController : MonoBehaviour {
 	private static ILogger logger = Debug.logger;
 	
 	public GameObject innerSphere;
 	public Renderer rend;
 	public Color lerpedColor;
-	public int threshold;
+	public float threshold; // range [0, 1]
 
-	public Text cursorPos;
-	public Text onTriggerDist;
+	// TEXT FIELDS
+	public Text cursor3DPos;
 	public Text innerSpherePos;
 	public Text transitionDistText;
-	public Text otherTransformPosText;
-	public Text outerInnerDeltaText;
+	public Text transformPosText;
+	public Text sizeTransDeltaText;
 
 	private float transitionDistance;
 	private Vector3 cursorDistance;
@@ -30,7 +50,7 @@ public class SphereOuterController : MonoBehaviour {
 	// Outer + Inner
 	private float outerInnerDelta = 0;
 
-	private float sizeTransDelta = 1; // to init the ball white
+	private float sizeTransDelta; // to init the ball white
 
 	// Cursor Size
 	private float cursorSize = 0;
@@ -40,8 +60,9 @@ public class SphereOuterController : MonoBehaviour {
 		rend = innerSphere.GetComponent<Renderer>();
 		lerpedColor = Color.white;
 		otherLocal = null;
-
-		logger.Log ("outerInnerDelta: ", outerInnerDelta);
+		var col = GetComponent<SphereCollider> ();
+		var outerRadius = col.radius;
+		Debug.LogFormat ("Outter Rad: {0}", outerRadius);
 	}
 
 	void Update () {
@@ -51,13 +72,12 @@ public class SphereOuterController : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
+		logger.Log ("ontroggerenter");
 		if (other.CompareTag ("Cursor")) {
-			logger.Log ("ontroggerenter");
 		}
 	}
 
 	void CalcDistances () {
-		// cursorDistance = Vector3.Distance (transform.position, otherLocal.transform.position);
 		cursor3dPos = new Vector3(otherLocal.transform.position.x, otherLocal.transform.position.y, otherLocal.transform.position.z);
 		inner3dPos = new Vector3 (innerSphere.transform.position.x, innerSphere.transform.position.y, innerSphere.transform.position.z);
 		transitionDistance = Vector3.Distance (cursor3dPos, inner3dPos);
@@ -72,12 +92,12 @@ public class SphereOuterController : MonoBehaviour {
 		// Cursor Size
 		cursorSize = otherLocal.transform.localScale.x;
 
-		sizeTransDelta = transitionDistance - outerInnerDelta + (cursorSize / 2);
+		sizeTransDelta = transitionDistance - outerInnerDelta + (cursorSize / 2); 
 	}
 
 	void SetColor () {
 		lerpedColor = Color.Lerp(Color.blue, Color.white, sizeTransDelta );
-		if (sizeTransDelta > 0) {
+		if (sizeTransDelta > threshold) {
 			rend.material.color = lerpedColor;
 		} else {
 			rend.material.color = Color.red;
@@ -98,13 +118,13 @@ public class SphereOuterController : MonoBehaviour {
 		sizeTransDelta = 1;
 		SetColor ();
 	}
-
+		
 	void UILog() {
-		otherTransformPosText.text = "other.transform.position: " + otherLocal.transform.position;
-		cursorPos.text = "cursor3dpos: " + cursor3dPos;
-		onTriggerDist.text = "transform.position: " + transform.position;
-		innerSpherePos.text = "inner3dpos: " + inner3dPos;
-		transitionDistText.text = "cursor3dPos -> inner3dPos: " + transitionDistance;
-		outerInnerDeltaText.text = "transitionDistance - outerInnerDelta: " + sizeTransDelta;
+		logger.Log("james was here");
+		cursor3DPos.text = ""+ cursor3dPos;
+		transformPosText.text = "" + transform.position;
+		innerSpherePos.text = "" + inner3dPos;
+		transitionDistText.text = "" + transitionDistance;
+		sizeTransDeltaText.text = "" + sizeTransDelta;
 	}
 }
--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # UX_Tasks
 UX Tasks
 
+## First Task Completed Notes
 
-### TASKS
+The task I completed first has some properties of the other tasks.
+Task:
+A sphere that changes from white to blue based on palm proximity, and red when within the near threshold.
+
+Instructions:
+
+Click anywhere on the screen with a mouse and the "Cursor" object will fly to the cursor. When the Cursor approaches the mouse an increase in drag is added until it comes to a stop.When it passes the threshold and you keep mousedown it will snap onto the mouse and manipulation of the objects transform will be used instead of adding force to attract it.
+
+You can then use the "Cursor" object to pass through the translucent outer sphere (I could have just used the Sphere Collider here) to change the inner sphere from white to blue then red when it hits the threshold.
+
+### TASK LIST
 
 - Grab a sphere, have it snap into a "seat" (upon release) if it's within X cm
 
@@ -16,9 +27,8 @@ UX Tasks
 
 ### Notes
 
-
 Stage 1 working `A sphere that changes from white to blue based on palm proximity, and red when within the near threshold`
 
-Created a colider that flys with momentum to the cursor then stops and follows 1 : 1 ratio when the cursor moves. 
+Created a colider that flys with momentum to the cursor then stops and follows 1 : 1 ratio when the cursor moves.
 
 This is for simulating the meta hands top point or palm colider.
