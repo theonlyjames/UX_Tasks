0333cc709fa3f4a94acb2320240bcda41d480398
theonlyjames <jkhatiblou@gmail.com>
2016-11-08 16:23:01 -0800

    Init

---
+++ Assets/_Scripts/SphereOuterController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereOuterController : MonoBehaviour {
+	
+	public GameObject innerSphere;
+
+	public Renderer rend;
+
+	void SetRenderer () {
+		rend = innerSphere.GetComponent<Renderer>();
+	}
+
+	void Start() {
+		//rend = GetComponent<Renderer>();
+		SetRenderer();
+	}
+
+	void OnMouseEnter() {
+		rend.material.color = Color.green;
+	}
+
+	void OnMouseOver() {
+		rend.material.color -= new Color(0.1F, 0, 0) * Time.deltaTime;
+	}
+
+	void OnMouseExit() {
+		rend.material.color = Color.white;
+	}
+}
