9527e823ddd82f0a49783ae090057f0f30493cce
theonlyjames <jkhatiblou@gmail.com>
2016-11-08 21:28:24 -0800

    Crating 3d threshold for calculating Inner - Outer Sphear + Cursor size / 2

--- Assets/_Scripts/SphereOuterController.cs
+++ Assets/_Scripts/SphereOuterController.cs
@@ -1,57 +1,102 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SphereOuterController : MonoBehaviour {
 	private static ILogger logger = Debug.logger;
 	
 	public GameObject innerSphere;
-
 	public Renderer rend;
+	public Color lerpedColor;
+	public int threshold;
 
-	public Color lerpedColor = Color.white;
+	public Text cursorPos;
+	public Text onTriggerDist;
+	public Text innerSpherePos;
+	public Text transitionDistText;
+	public Text otherTransformPosText;
+	public Text outerInnerDeltaText;
 
-	private float enterPoint;
+	private float transitionDistance;
+	private Vector3 cursorDistance;
+	private Collider otherLocal;
 
-	private Material innerSphereMaterial;
+	// Sphear Sizes to calculate edges
+	// Outer
+	private float outerSize;
+	// Innner
+	private float innerSize;
+	private Vector3 inner3dPos;
+	// Outer + Inner
+	private float outerInnerDelta = 0;
 
-	private float cursorDistance;
+	// Cursor Size
+	private float cursorSize = 0;
+	private Vector3 cursor3dPos;
 
 	void Start() {
-		SetRenderer();
-		innerSphereMaterial = innerSphere.GetComponent<Material> ();
+		rend = innerSphere.GetComponent<Renderer>();
+		lerpedColor = Color.white;
+		otherLocal = null;
+
+		logger.Log ("outerInnerDelta: ", outerInnerDelta);
 	}
 
-	void SetRenderer () {
-		rend = innerSphere.GetComponent<Renderer>();
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			logger.Log ("outerInnerDelta: ", outerInnerDelta);
+		}
 	}
 
 	void OnTriggerEnter(Collider other) {
 		if (other.CompareTag ("Cursor")) {
-			Debug.Log ("ontroggerenter");
-			enterPoint = Vector3.Distance (transform.position, other.transform.position);
+			logger.Log ("ontroggerenter");
+		}
+	}
+
+	void CalcDistances () {
+		// cursorDistance = Vector3.Distance (transform.position, otherLocal.transform.position);
+		cursor3dPos = new Vector3(otherLocal.transform.position.x, otherLocal.transform.position.y, otherLocal.transform.position.z);
+		inner3dPos = new Vector3 (innerSphere.transform.position.x, innerSphere.transform.position.y, innerSphere.transform.position.z);
+		transitionDistance = Vector3.Distance (cursor3dPos, inner3dPos);
+	}
+
+	void CalcSphereDelta() {
+		// Calc size use x, y, or z because it's a sphear.
+		outerSize = transform.localScale.x;
+		innerSize = innerSphere.transform.localScale.x;
+		outerInnerDelta = (outerSize - innerSize) / 2;
+
+		// Cursor Size
+		cursorSize = otherLocal.transform.localScale.x;
+	}
+
+	void SetColor () {
+		lerpedColor = Color.Lerp(Color.white, Color.blue, transitionDistance - outerInnerDelta + cursorSize );
+		if (transitionDistance > 0) {
+			rend.material.color = lerpedColor;
+		} else {
+			rend.material.color = Color.red;
 		}
 	}
 
 	void OnTriggerStay(Collider other) {
+		otherLocal = other;
 		if (other.CompareTag ("Cursor")) {
-			Debug.logger.Log ("outer pos", transform.position);
-			Debug.logger.Log ("inner pos", innerSphere.transform.position);
-			Debug.logger.Log ("cursor dis", cursorDistance);
-			cursorDistance = Vector3.Distance (transform.position, other.transform.position);
-			lerpedColor = Color.Lerp(Color.green, Color.red, cursorDistance - innerSphere.transform.position.y);
-			rend.material.color = lerpedColor;
+			CalcDistances ();
+			SetColor ();
+			CalcSphereDelta ();
+			UILog ();
 		}
 	}
 
-//	void OnMouseEnter() {
-//		rend.material.color = Color.green;
-//	}
-
-//	void OnMouseOver() {
-//		rend.material.color -= new Color(0.1F, 0, 0) * Time.deltaTime;
-//	}
-
-//	void OnMouseExit() {
-//		rend.material.color = Color.white;
-//	}
+	void UILog() {
+		otherTransformPosText.text = "other.transform.position: " + otherLocal.transform.position;
+		cursorPos.text = "cursor3dpos: " + cursor3dPos;
+		onTriggerDist.text = "transform.position: " + transform.position;
+		innerSpherePos.text = "inner3dpos: " + inner3dPos;
+		transitionDistText.text = "cursor3dPos -> inner3dPos: " + transitionDistance;
+		float sizeTransDelta = transitionDistance - outerInnerDelta + cursorSize;
+		outerInnerDeltaText.text = "transitionDistance - outerInnerDelta: " + sizeTransDelta;
+	}
 }
