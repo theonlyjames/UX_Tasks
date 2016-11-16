ce49298b922651df50099ef2110e19ea1e2f3934
theonlyjames <jkhatiblou@gmail.com>
2016-11-16 15:02:21 -0800

    Changing the inner circle to red on enter working

---
+++ Assets/_Scripts/InnerSphereController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InnerSphereController : MonoBehaviour {
+
+	private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("Cursor")) {
+			rend.material.color = Color.red; 
+		}
+	}
+}
